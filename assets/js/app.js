6069db5cbe2c2b69eb18b63fd2c8a0658d44a10d
Clara Márquez <clara.marquez.torres@gmail.com>
2023-04-20 00:16:32 +0200

    Players 19.04

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,20 @@
-console.log('Hello World!');
+window.addEventListener("load", () => {
+    let team = document.querySelectorAll(".card div");
+    let dissables = document.querySelectorAll(".dissable");
+    let buttons = document.querySelectorAll(".buttonDissable");
+    
+    team.forEach((player, index) => {
+        let dissable = dissables[index];
+        let button = buttons[index];
+
+        player.addEventListener("mouseover", () => {
+        dissable.classList.remove("dissable");
+        button.classList.add("buttonDissable");
+        });
+
+        player.addEventListener("mouseout", () => {
+        dissable.classList.add("dissable");
+        button.classList.remove("buttonDissable");
+        });
+    });
+});
