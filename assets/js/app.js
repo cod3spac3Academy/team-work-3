0cbac2227740232837f615e34158505c27210152
Giselle Cole <giselle.cole@agnos.ai>
2023-04-19 03:01:13 +0200

    working on desktop

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,51 @@
-console.log('Hello World!');
+window.addEventListener("load", () => {
+  let slideIndex = 1;
+  showSlides(slideIndex);
+
+  function currentSlide(n) {
+    showSlides((slideIndex = n));
+  }
+
+  function showSlides(n) {
+    let i;
+    let slides = document.querySelectorAll(".mySlides");
+    let dots = document.querySelectorAll(".dot");
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].classList.remove("active");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].classList.add("active");
+  }
+
+  let dotElements = document.querySelectorAll(".dot");
+  for (let i = 0; i < dotElements.length; i++) {
+    dotElements[i].addEventListener("click", () => {
+      currentSlide(i + 1);
+    });
+  }
+    // automatic Slider
+//     let slideIndex = 0;
+// showSlides();
+
+// function showSlides() {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}
+//   slides[slideIndex-1].style.display = "block";
+//   setTimeout(showSlides, 2000); // Change image every 2 seconds
+// }
+
+});
