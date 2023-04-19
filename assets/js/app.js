03083635685fee999d56e2f6031ffda62f6476db
javier <jbotellalecanda@gmail.com>
2023-04-19 17:14:53 +0200

    mercredi

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,18 @@
-console.log('Hello World!');
+let header = document.querySelector("header");
+    // window.addEventListener("scroll",(e)=>{
+    //     header.classList.add("hide")
+    // })
+    let InitialScrollPos = window.pageYOffset; //almacenamos la posición inicial del scroll
+    window.addEventListener("scroll", () => {
+      const currentScrollPos = window.pageYOffset; //almacenamos la posición actual del scroll
+      if (InitialScrollPos > currentScrollPos) {
+        //scroll hacia arriba
+        //mostrar el header si se ocultó
+        header.classList.remove("hide");
+      } else {
+        //scroll hacia abajo
+        //ocultar el header
+        header.classList.add("hide");
+      }
+      InitialScrollPos = currentScrollPos; //actualizamos la posición anterior del scroll
+    });
