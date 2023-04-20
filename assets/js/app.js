84b0ef1c64630953bef078796f2a327beeb19356
Giselle Cole <giselle.cole@agnos.ai>
2023-04-20 20:16:41 +0200

    added const in js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,55 @@
 window.addEventListener("load", () => {
-    // automatic Slider
-let slideIndex = 0;
-showSlides();
+  const slideManual = () => {
+    let slideIndex = 1;
+    showSlides(slideIndex);
+    function currentSlide(n) {
+      showSlides((slideIndex = n));
+    }
+    function showSlides(n) {
+      let i;
+      let slides = document.querySelectorAll(".slideShow");
+      let dots = document.querySelectorAll(".dot");
+      if (n > slides.length) {
+        slideIndex = 1;
+      }
+      if (n < 1) {
+        slideIndex = slides.length;
+      }
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      for (i = 0; i < dots.length; i++) {
+        dots[i].classList.remove("active");
+      }
+      slides[slideIndex - 1].style.display = "block";
+      dots[slideIndex - 1].classList.add("active");
+    }
+    let dotElements = document.querySelectorAll(".dot");
+    for (let i = 0; i < dotElements.length; i++) {
+      dotElements[i].addEventListener("click", () => {
+        currentSlide(i + 1);
+      });
+    }
+  };
+  // automatic Slider
+  const slideAuto = () => {
+    let slideIndex = 0;
+    showSlides();
 
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("slideShow");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
+    function showSlides() {
+      let i;
+      let slides = document.getElementsByClassName("slideShow");
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) {
+        slideIndex = 1;
+      }
+      slides[slideIndex - 1].style.display = "block";
+      setTimeout(showSlides, 2000); // Change image every 2 seconds
+    }
+  };
+  slideManual();
+  // slideAuto();
 });
