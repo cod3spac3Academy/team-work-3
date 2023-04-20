ad20a9cd526ec3b2325cc8bc0d8d209ea3600247
Juanmi7 <125692808+Juanmi7@users.noreply.github.com>
2023-04-20 18:44:06 +0200

    Team3 (#5)
    * version movil css y html casi listo rama games casa
    
    * parte de juegos lista para mergear
    
    * games ready

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,7 @@
-console.log('Hello World!');
+const gameDivs = document.querySelectorAll("section.games .newGames");
+
+for (let i = 0; i < gameDivs.length; i++) {
+  gameDivs[i].addEventListener("click", function () {
+    window.location.href = gameDivs[i].dataset.link;
+  });
+}
